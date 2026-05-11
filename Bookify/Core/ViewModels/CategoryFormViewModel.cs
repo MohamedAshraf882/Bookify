0cee8855dc7a985293f37c2990fd475ec3577d9b
Mohamed Ashraf <mohamednokara121@gmail.com>
2026-05-11 05:59:30 +0300

    -add categories (creat view,edit view) -add formviewmodel for create and edit -add javascript validation style -edit logo -add icon font

---
+++ Bookify/Core/ViewModels/CategoryFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Bookify.Core.ViewModels
+{
+    public class CategoryFormViewModel
+    {
+
+        public int ID { get; set; }
+        [MaxLength(100, ErrorMessage = "Max length Cannot be more 100 chr.")]
+        public string Name { get; set; }
+    }
+}
--- Bookify/GlobalUsing.cs
+++ Bookify/GlobalUsing.cs
@@ -1,1 +1,4 @@
 ﻿global using System.ComponentModel.DataAnnotations;
+global using Bookify.Core.ViewModels;
+global using Bookify.Data;
+global using Bookify.Core.Models;
